3ea7d4ca1a3663fe8f6a76690810b8ae1ee1ff34
13city <6west@protonmail.com>
2023-03-06 16:06:51 -0500

    Initial Push

---
+++ src/components/footer.jsx
@@ -0,0 +1,17 @@
+export const Footer = (props) => {
+  
+  return (
+    <div>
+      <div id='footer'>
+        <div className='container text-center'>
+          <p>
+            &copy; Global Technical Consultancy . Design by{' '}
+            <a href='' rel='nofollow'>
+              GTC
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
